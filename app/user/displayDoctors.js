57ce97aa3dae0bbdc4334270630e5b6c97ea03cb
Mohit Kumar Agrawal <mhtkrag@gmail.com>
2023-04-14 13:17:52 +0530

    distance feature

--- app/user/displayDoctors.js
+++ app/user/displayDoctors.js
@@ -5,10 +5,30 @@ const pool = require("../db");
 
 const displayDoctors = async (req, res) => {
   try {
-    //console.log("into display doctors");
+    console.log("into display doctors");
     const { speciality } = req.body;
+
+    let lat = req.body.lat;
+    let lng = req.body.lng;
+
+    if (!lat) lat = "0";
+    if (!lng) lng = "0";
+
+    console.log("running query");
+
     const doctors = await pool.query(
-      "select doctors.id as doctor_id,doctors.name as doctor_name,doctors.email as doctor_email,qualifications,speciality,doctors.hospital_id as hospital_id,hospitals.name as hospital_name,address from doctors,hospitals where doctors.hospital_id=hospitals.id and speciality=?",
+      `select 
+          doctors.id as doctor_id, 
+          doctors.name as doctor_name, 
+          doctors.email as doctor_email, 
+          qualifications, 
+          speciality, 
+          doctors.hospital_id as hospital_id,
+          hospitals.name as hospital_name,
+          address,
+          st_distance_sphere(st_geomfromtext("POINT(${lng} ${lat})", 4326), coords) as distance
+      from doctors,hospitals 
+      where doctors.hospital_id=hospitals.id and speciality=?`,
       [speciality]
     );
     //console.log(doctors);
